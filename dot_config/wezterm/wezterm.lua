3b6d1b5a9b1d2c2c7728fcf139daa5ee37ce0410
Walid Amro <walidamr.work@gmail.com>
2025-09-27 23:04:29 +0300

    added more config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- config.color_scheme = 'Batman'
+config.font = wezterm.font 'Fira Code'
+-- config.harfbuzz_features = { 'curs=0', 'clig=0' }
+
+return config
